c4684259068480de5b820e9951ba80fdc5af4ae7
Randy Can <canalex210@gmail.com>
2021-02-15 19:51:55 -0600

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { UserInfComponent } from './components/user-inf/user-inf.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'Login'
+  },
+  {
+    path:'Login',
+    component:LoginComponent
+  },
+  {
+    path:'Informacion',
+    children:[
+      {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'Registro'
+      },
+      {
+        path:'Registro',
+        component:UserInfComponent
+      },
+      {
+        path:'view',
+        component:UserInfComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
